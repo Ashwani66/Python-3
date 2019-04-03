d61579f48f49dd020ee81c13ce0d4cc4727a39f3
Ashwani singh <43879028+Ashwani66@users.noreply.github.com>
2019-04-03 07:00:52 +0530

    Create Python-Variable.py

---
+++ Python-Variable.py
@@ -0,0 +1,13 @@
+"""Creating Variables
+
+Unlike other programming languages, Python has no command for declaring a variable.
+
+A variable is created the moment you first assign a value to it."""
+x = 5
+y = "John"
+print(x)
+print(y)
+"""Variables do not need to be declared with any particular type and can even change type after they have been set."""
+x = 4
+x = "Sally"
+print(x)
