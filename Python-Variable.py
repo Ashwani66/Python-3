035bc27e70c3b4586c95feef76ad65da03fc9506
Ashwani singh <43879028+Ashwani66@users.noreply.github.com>
2019-04-13 18:25:25 +0530

    Update Python-Variable.py

--- Python-Variable.py
+++ Python-Variable.py
@@ -2,7 +2,7 @@
 
 Unlike other programming languages, Python has no command for declaring a variable.
 
-A variable is created the moment you first assign a value to it."""
+A variable is created the moment you first assign a value to   it."""
 x = 5
 y = "John"
 print(x)
