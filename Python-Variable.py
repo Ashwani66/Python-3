658cc2486f84add80ee2b175abde494468b21516
Ashwani singh <43879028+Ashwani66@users.noreply.github.com>
2019-04-13 18:23:39 +0530

    Update Python-Variable.py

--- Python-Variable.py
+++ Python-Variable.py
@@ -7,7 +7,7 @@ x = 5
 y = "John"
 print(x)
 print(y)
-"""Variables do not need to be declared with any particular type and can even change type after they have been set."""
+"""Variables do   not need to be declared with any particular type and can even change type after they have been set."""
 x = 4
 x = "Sally"
 print(x)
